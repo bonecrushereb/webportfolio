757c24fa5950e76714925b92f1a2bc51bb18ac5b
bonecrushereb <bonecrushereb@gmail.com>
2016-08-26 11:22:36 -0700

    modified server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const webprojectRouter = require(__dirname + '/routes/webprojectrouter');
 const gameprojectRouter = require(__dirname + '/routes/gameprojectrouter');
 const badgeRouter = require(__dirname + '/routes/badgerouter');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOD_URI ||
+mongoose.connect(process.env.MONGODB_URI ||
 'mongodb://localhost/projectbadge_db');
 
 app.use('/api', webprojectRouter);
