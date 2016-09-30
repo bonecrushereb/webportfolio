c422f764990a1b18ae49a94a94ee06cf2c0cc485
bonecrushereb <bonecrushereb@gmail.com>
2016-09-30 12:05:21 -0700

    working on sass

--- models/gameproject.js
+++ models/gameproject.js
@@ -4,7 +4,8 @@ var gameprojectSchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true },
   author: { type: String, required: true },
   body: { type: String, required: true },
-  img: { type: String, required: true }
+  img: { type: String, required: true },
+  techused: { type: String, required: true }
 });
 
 module.exports = exports = mongoose.model('gameproject', gameprojectSchema);
--- models/webproject.js
+++ models/webproject.js
@@ -5,7 +5,9 @@ var webprojectSchema = new mongoose.Schema({
   author: { type: String, required: true },
   authorUrl: { type: String, required: true },
   body: { type: String, required: true },
-  img: { type: String, required: true }
+  img: { type: String, required: true },
+  techused: { type: String, required: true }
+
 });
 
 module.exports = exports = mongoose.model('Webproject', webprojectSchema);
