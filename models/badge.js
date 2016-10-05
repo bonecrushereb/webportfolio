c61af6112f9f61de052d061f09fc4be36b341744
bonecrushereb <bonecrushereb@gmail.com>
2016-10-05 14:29:28 -0700

    reworked badge for skills page

--- models/badge.js
+++ models/badge.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 var badgeSchema = new mongoose.Schema({
   title: { type: String, unique: true, required: true },
-  body: { type: String, required: true },
-  img: { type: String, required: true }
+  skill: { type: String, required: true }
 });
 
-module.exports = mongoose.model('badge', badgeSchema)
+module.exports = mongoose.model('badge', badgeSchema);
