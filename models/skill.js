ca2a0ab5db935f41e271eb3f2b5738c3ed3e1d5e
bonecrushereb <bonecrushereb@gmail.com>
2016-10-10 16:37:16 -0700

    templated skill

--- models/skill.js
+++ models/skill.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-var badgeSchema = new mongoose.Schema({
+var skillSchema = new mongoose.Schema({
   career: { type: String, unique: true, required: true },
   skill: { type: String, unique: true, required: true }
 });
 
-module.exports = mongoose.model('badge', badgeSchema)
+module.exports = mongoose.model('skill', skillSchema)
