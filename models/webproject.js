8cd8ff0b7d0c4ad898f4faead2ba0efc86a1b82e
bonecrushereb <bonecrushereb@gmail.com>
2016-11-14 14:32:09 -0800

    fixed models for projects

--- models/webproject.js
+++ models/webproject.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 var webprojectSchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true },
-  author: { type: String, required: true },
-  authorUrl: { type: String, required: true },
+  author: { type: String },
+  authorUrl: { type: String },
   body: { type: String, required: true },
   img: { type: String, required: true },
-  techused: { type: String, required: true }
+  techused: { type: String }
 
 });
 
