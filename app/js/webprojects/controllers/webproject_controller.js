c3443893d7c1d8b5450f92f1d57067d16079e0db
bonecrushereb <bonecrushereb@gmail.com>
2016-12-17 14:01:19 -0800

    reformated skills page and project pages

--- app/js/webprojects/controllers/webproject_controller.js
+++ app/js/webprojects/controllers/webproject_controller.js
@@ -1,13 +1,26 @@
 const angular = require('angular');
 var baseUrl = require('../../config').baseUrl;
+// var imageUrl = '../images/wavebackground.jpg';
 
 module.exports = function(app) {
-  app.controller('WebProjectsController', ['pResource', 'pStore', function(Resource, pStore) {
+  app.controller('WebProjectsController', ['pResource', 'pStore', '$location',
+  function(Resource, pStore, $location) {
     this.pStore = pStore;
     this.webprojects = pStore.webprojects;
     this.addWebProject = pStore.addWebProject.bind(pStore);
     this.errors = [];
     this.remote = new Resource(this.webprojects, this.errors, baseUrl + '/api/webprojects');
+
+    if ($location.path() === '/') {
+      angular.element(document.querySelector('.main'))
+          .css({
+            background: 'url("../images/wavebackground.jpg") no-repeat center',
+            'background-size': 'cover'
+          });
+    } else {
+      angular.element(document.querySelector('.main')).css({ background: '#f6da90' });
+    }
+
     this.getAll = this.remote.getAll.bind(this.remote);
 
     this.createWebProject = function() {
